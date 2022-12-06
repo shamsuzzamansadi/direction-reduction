b90b28a5f79f76212b41279ec380af66c6656113
unknown <abmsadi06@gmail.com>
2022-12-06 16:16:38 +0100

    reduced the code size

--- src/Controller/DirectionController.php
+++ src/Controller/DirectionController.php
@@ -9,61 +9,37 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DirectionController extends AbstractController
 {
-    #[Route('/direction', methods:['GET','HEAD'], name: 'app_direction')]
+    #[Route('/direction={requests}', methods:['GET','HEAD'], name: 'app_direction')]
 
-    public function dirReduc(): Response
+    public function dirReduc($requests ): Response
     {
-        //  /**
-        //   * @var request Symfony\Component\HttpFoundation\Request
-        //   */
-        // $directions = $requests->request->all();
-        // $directions = array($directions);
-        $directions = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST", "NORTH"];
+         /**
+          * @var Symfony\Component\HttpFoundation\Request
+          */
+        $directions = $requests->request->all();
+        $directions = array($directions);
+        var_dump($directions);
+        // $directions = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST", "NORTH"];
        
 
+        $reduction_mapping = [
+            'NORTH' => 'SOUTH',
+            'SOUTH' => 'NORTH',
+            'WEST' => 'EAST',
+            'EAST' => 'WEST'
+                
+        ];
+            
         $result = [];
+        
         foreach($directions as $direction){
-            if(!$result){
-                array_push($result,$direction);
-                
-            }
-            else{
-                if(end($result) == "NORTH"){
-                    if($direction == "SOUTH"){
-                        array_pop($result);
-                    }
-                    else{
-                        array_push($result,$direction);
-                    }
-                }
-                elseif (end($result) == "SOUTH") {
-                    if($direction == "NORTH"){
-                        array_pop($result);
-                    }
-                    else{
-                        array_push($result,$direction);
-                    }
-                }
-                elseif (end($result) == "EAST") {
-                    if($direction == "WEST"){
-                        array_pop($result);
-                    }
-                    else{
-                        array_push($result,$direction);
-                    }
-                }
-                elseif (end($result) == "WEST") {
-                    if($direction == "EAST"){
-                        array_pop($result);
-                    }
-                    else{
-                        array_push($result,$direction);
-                    }
-                }
-                
-                // array_push($result[$direction],$directions);
+            if($result && $reduction_mapping[$direction] === end($result)){
+                array_pop($result);
+            } else {
+                array_push($result, $direction);
             }
         }
+        
 
         return $this->json( [
             "result" => $result
