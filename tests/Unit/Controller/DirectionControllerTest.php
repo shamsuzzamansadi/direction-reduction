535f6948ced070836e282505ee0382868802f230
unknown <abmsadi06@gmail.com>
2022-12-09 14:07:09 +0100

    [WIP]unit test basic fixes

---
+++ tests/Unit/Controller/DirectionControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\Unit\Controller;
+
+use App\Controller\DirectionController;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
+class DirectionControllerTest extends TestCase
+{
+    protected ContainerInterface $cotainer;
+
+    /** @test */
+    public function dirReducTest(): void
+    {
+        
+        $directionController = new DirectionController();
+ 
+    
+        $this->assertEquals(["WEST","NORTH"],$directionController->dirReduc("NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST", "NORTH"));
+        $this->assertEquals([],$directionController->dirReduc("NORTH","SOUTH","SOUTH","EAST","WEST","NORTH"));
+        $this->assertEquals(["NORTH"],$directionController->dirReduc("NORTH","SOUTH","SOUTH","EAST","WEST","NORTH","NORTH"));
+
+    }
+}
